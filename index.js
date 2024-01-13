5a32091cb1486820f188e63835bca0fbca541ece
ARMcPro <92748428+ARMcPro@users.noreply.github.com>
2024-01-13 17:01:17 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,113 @@
+const express = require('express');
+require("dotenv").config();
+run = ""
+
+const app = express();
+const fs = require('fs');
+
+const resrictedPlayers = ['0'];
+
+var old_timestamp = 10;
+
+const sleep = (milliseconds) => {
+  return new Promise(resolve => setTimeout(resolve, milliseconds))
+};
+
+app.use(express.static(__dirname));
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.get('/hints', (req, res) => {
+  res.sendFile(__dirname + "/hints.html");
+});
+
+/*
+function fetchName(uuid) {
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open( "GET", "https://api.mojang.com/user/profile/" + uuid, false );
+    xmlHttp.send( null );
+    return JSON.parse(xmlHttp.responseText)["name"];
+}*/
+
+async function fetchName(uuid) {
+  return await fetch(`https://api.mojang.com/user/profile/${uuid}`)
+    .then((response) => response.json())
+}
+
+
+app.post('/post', express.json(), (req,res) => {
+  console.log(req.body);
+  try {
+    const P = 'P';
+    let timestamp = new Date().getTime() / 1000;
+    if (timestamp >= 1686988800 && timestamp < 1687161601) {
+      let plotID = req.headers['user-agent'].split(' (')[1].split(', ')[0];
+      console.log(plotID);
+      fs.readFile("./data/plotdata.json", async function (err, data) {
+        var json = JSON.parse(data);
+        if (typeof json[plotID] !== 'undefined' && typeof req.body['point'] === 'string' && typeof req.body['uuid'] === 'string' && req.body['key'] === process.env[P + plotID] && !resrictedPlayers.includes(req.body['uuid']) && typeof process.env[P + plotID] === 'string') {
+          console.log("1");
+          if (json[plotID].includes(req.body['point'].toLowerCase())) {
+            fs.readFile("./data/playerdata.json", async function (perr, pdata) {
+              var pjson = JSON.parse(pdata);
+              if (typeof pjson[req.body['uuid']] === 'undefined')             {
+                console.log("new");
+                var pname = await fetchName(req.body['uuid'])["name"];
+                if (typeof pname !== 'undefined') {
+                  var keyName = req.body['uuid']
+                  pjson[keyName] = [pname, req.body['point'].toLowerCase()];
+                  fs.writeFileSync("./data/playerdata.json", JSON.stringify(pjson));
+                };
+              }
+              else if (!pjson[req.body['uuid']].includes(req.body['point'].toLowerCase()))        {
+                console.log("old");
+                var keyName = req.body['uuid'];
+                var pname = await fetchName(req.body[keyName])["name"];
+                pjson[keyName].push(req.body['point'].toLowerCase());
+                fs.writeFileSync("./data/playerdata.json", JSON.stringify(pjson));
+              };
+            });
+          };
+        };
+      });
+    };
+  }
+  catch(err) {
+    console.log(err);
+  };
+  res.send();
+});
+
+
+app.get('/sync', async (req, res) => {
+  if (!typeof old_timestamp === 'number') {
+    old_timestamp = 10;
+  };
+  let timestamp = new Date().getTime();
+  res.sendFile(__dirname + "/index.html");
+  if ((timestamp - old_timestamp) >= 86400000) {
+    console.log(old_timestamp);
+    old_timestamp = new Date().getTime() + 86400000;
+    fs.readFile("./data/playerdata.json", async function (perr, pdata) {
+      const pjson = JSON.parse(pdata);
+      for (const [key, value] of Object.entries(pjson)) {
+        let newName = await fetchName(key);
+        if (typeof newName["name"] === 'string'){
+          pjson[key][0] = newName["name"];
+          await sleep(250);
+        };
+      };
+      fs.writeFileSync("./data/playerdata.json", JSON.stringify(pjson));
+      console.log("Successfully Synced!")
+    });
+    old_timestamp = new Date().getTime();
+  };
+});
+
+
+app.listen(8080, () => {
+  console.log('Server started at ' + new Date().toUTCString())
+});
+
---
+++ website.js
@@ -0,0 +1,155 @@
+function sortPlayerData(myDict){
+  const sortedArray = Object.entries(myDict)
+  .map(([key, value]) => [key, value[0], value.length-1])
+  .sort((a, b) => b[2] - a[2] || a[0] - b[0])
+  .map(([key, firstValue, lengthMinusOne], index) => [index + 1, firstValue, lengthMinusOne]);
+  var data = sortedArray;
+  data.unshift(['#','Name','Diamonds']);
+  const table = document.createElement('table');
+  table.setAttribute("id", "content-table");
+  if (data.length === 1) {
+    data.push(['–','–','–'])
+  };
+
+  const headerRow = table.insertRow();
+  data[0].forEach((header) => {
+    const headerCell = headerRow.insertCell();
+    headerCell.textContent = header;
+    headerCell.classList.add('boldHeader');
+  });
+
+  for (let i = 1; i < data.length; i++) {
+    const row = table.insertRow();
+    data[i].forEach((cell, index) => {
+      const cellElement = row.insertCell();
+      cellElement.textContent = cell;
+    });
+  }
+  
+  const container = document.getElementById('table-container');
+  container.appendChild(table);
+};
+
+function getPlayerData(playerData, PDATA){
+  var result = {};
+  for (const key in PDATA) {
+    const count = PDATA[key].filter(item => playerData.slice(1).includes(item)).length;
+    if (count > 0) {
+      result[key] = count;
+    }
+  }
+  console.log(result)
+  var sortedArray = Object.entries(result)
+  .map(([key, value]) => [key, key, value])
+  .sort((a, b) => a[2] - b[2] || a[0] - b[0])
+  .map(([_, firstValue, lengthMinusOne], index) => [index + 1, firstValue, lengthMinusOne]);
+  var data = sortedArray;
+  data.unshift(['#','Plot ID','Diamonds']);
+  const table = document.createElement('table');
+  table.setAttribute("id", "content-table");
+  if (data.length === 1) {
+    data.push(['–','–','–'])
+  };
+
+  const headerRow = table.insertRow();
+  data[0].forEach((header) => {
+    const headerCell = headerRow.insertCell();
+    headerCell.textContent = header;
+    headerCell.classList.add('boldHeader');
+  });
+
+  for (let i = 1; i < data.length; i++) {
+    const row = table.insertRow();
+    data[i].forEach((cell, index) => {
+      const cellElement = row.insertCell();
+      cellElement.textContent = cell;
+    });
+  }
+  
+  const container = document.getElementById('table-container');
+  container.appendChild(table);
+};
+
+function findPlayerKey(obj, str) {
+  for (let key in obj) {
+    if (obj.hasOwnProperty(key)) {
+      if (obj[key][0].toLowerCase() === str.toLowerCase()) {
+        return key;
+      }
+    }
+  }
+  return null;
+};
+
+function storePlayerData(playerData){
+  playersjson = playerData;
+};
+
+function storePlotData(plotData){
+  plotsjson = plotData;
+};
+
+
+fetch('data/playerdata.json')
+  .then(response => response.text())
+  .then(data => {
+    storePlayerData(JSON.parse(data));
+    sortPlayerData(JSON.parse(data));
+  })
+  .catch(error => console.error(error));
+
+fetch('data/plotdata.json')
+  .then(response => response.text())
+  .then(data => {
+    storePlotData(JSON.parse(data));;
+  })
+  .catch(error => console.error(error));
+
+var searchbox = document.getElementById("searchbox");
+
+function processInput() {
+  var value = searchbox.value.trim();
+  document.getElementById("content-table").remove();
+  if (value === ''){
+    sortPlayerData(playersjson);
+    console.log('clear');
+  } else {
+    var playerName = findPlayerKey(playersjson, value);
+    if (playerName === null) {
+      console.log('no data found');
+      const table = document.createElement('table');
+      table.setAttribute("id", "content-table");
+      const headerRow = table.insertRow(0);
+      const headerCell = headerRow.insertCell(0);
+      headerCell.textContent = "No Data Found";
+      headerCell.classList.add('redHeader');
+      console.log(table);
+      const container = document.getElementById('table-container');
+      container.appendChild(table);
+      return
+    } else {
+      console.log(playerName)
+      getPlayerData(playersjson[playerName],plotsjson);
+    }
+    console.log(value);
+  };
+}
+
+searchbox.addEventListener("keyup", function(event) {
+  if (event.key === "Enter") {
+    processInput();
+  }
+});
+
+var submitSearch = document.getElementById("submit-search");
+submitSearch.addEventListener("click", function() {
+  submitSearch.classList.add("clicked");
+  processInput();
+});
+
+var clearSearch = document.getElementById("clear-search");
+clearSearch.addEventListener("click", function() {
+  clearSearch.classList.add("clicked");
+  searchbox.value = "";
+  processInput();
+});
