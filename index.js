7c7af587fc0b5ba484ed1f7e53d8519299858e88
ARMcPro <pdarhax@hotmail.com>
2024-05-29 20:50:34 +0300

    Cards?

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ run = ""
 
 const app = express();
 const fs = require('fs');
+const {createCanvas, loadImage, registerFont} = require('canvas');
+
 
 const resrictedPlayers = ['0'];
 
@@ -12,6 +14,122 @@ var old_timestamp = 10;
 const sleep = (milliseconds) => {
   return new Promise(resolve => setTimeout(resolve, milliseconds))
 };
+
+async function generateImage({playerName, placement, potatoesCollected, playerUUID}) {
+  // Constants
+  const width = 128;
+  const height = 128;
+  const canvas = createCanvas(width, height);
+  const ctx = canvas.getContext('2d');
+
+  const placementColors = ["#FAEE4D", "#FFFFFF", "#D87F33", "#A4A8B8"]
+  const placementShadowColors = ["#ba8524", "#545760", "#9f5224", "#4c4c4c"]
+  const placementLastIndex = placementColors.length-1;
+
+  // Format parameters
+  potatoesCollected = formatNumber(potatoesCollected)
+  placement = formatNumber(placement);
+  playerUUID = playerUUID === undefined ? '' : playerUUID;
+  playerName = playerName === undefined ? 'error' : playerName;
+
+  // Make rendering pixel-perfect
+  ctx.antialias = 'none'; // Text
+  ctx.quality = 'best';
+  ctx.textRendering = "optimizeLegibility";
+  ctx.imageSmoothingEnabled = false; // Images
+
+  // Register the font we are going to be using for the entire image
+  registerFont(`${__dirname}/Minecraft_Small_Caps.otf`, {family: 'Minecraft'});
+
+  // -- Image Rendering --
+
+  // Load the background image and draw it
+  const backgroundImage = await loadImage(__dirname + '/background.png');
+  ctx.drawImage(backgroundImage, 0, 0, width, height);
+
+  // Load the placement player head frame
+  const playerHeadFrame = await loadImage(__dirname + `/profile_frames/${placement >= 4 ? 4 : placement}.png`)
+  ctx.drawImage(playerHeadFrame, 0, 0, width, height)
+
+  // Draw player head
+  try {
+      const response = await fetch(`https://mc-heads.net/avatar/${playerUUID}`);
+      const buffer = await response.arrayBuffer();
+      const img = await loadImage(Buffer.from(buffer));
+      ctx.drawImage(img, width / 2 - 16, 28, 32, 32);
+  } catch (err) {
+      console.log("Error fetching image -- Background makes sure it's replaced with a checkerboard pattern.")
+      console.error(err);
+  }
+
+  // -- Text Rendering --
+
+  // Draw player name
+  //  Measure player name pixel width with 18px size, if it does not fit scale down to 9px
+  ctx.font = '18px Minecraft'
+  if (ctx.measureText(playerName).width >= width - 4) ctx.font = '9px Minecraft'
+  drawShadowText(ctx, playerName, width / 2, 15, "center")
+
+  // Draw statistics
+  ctx.font = 'bold 18px Minecraft';
+  drawShadowText(ctx, potatoesCollected, width, 81, "right")
+  drawColoredShadowText(ctx, `#${placement}`,
+      placementColors[placement - 1] === undefined ? placementColors[placementLastIndex] : placementColors[placement - 1],
+      placementShadowColors[placement - 1] === undefined ? placementShadowColors[placementLastIndex] : placementShadowColors[placement - 1],
+      width, 102, "right");
+
+  // Draw timestamp
+  ctx.font = '9px Minecraft'
+  drawShadowText(ctx, getFormattedDateList()[0], 1, 120)
+  drawShadowText(ctx, getFormattedDateList()[1], 1, 126)
+
+  // Get image buffer and save to file
+  return canvas.toBuffer('image/png');
+}
+
+// Add commas to numbers (1000 -> 1,000)
+function formatNumber(x) {
+  return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
+
+function drawColoredShadowText(ctx, text, color, shadowColor, x, y, alignment) {
+  const textWidth = ctx.measureText(text).width;
+
+  // Calculate the x position based on the alignment
+  let posX;
+  if (alignment === 'right') {
+      posX = x - textWidth - 4;
+  } else if (alignment === 'center') {
+      posX = ctx.canvas.width / 2 - textWidth / 2;
+  } else {
+      // Default to left alignment
+      posX = x;
+  }
+
+  // Draw shadow
+  ctx.fillStyle = shadowColor;
+  ctx.fillText(text, posX + 1, y + 1);
+
+  // Draw text
+  ctx.fillStyle = color;
+  ctx.fillText(text, posX, y);
+}
+
+function drawShadowText(ctx, text, x, y, alignment) {
+  drawColoredShadowText(ctx, text, 'white', '#545760', x, y, alignment)
+}
+
+
+// Returns a list containing the formatted date at index 0 and the formatted hour at index 1 (UTC)
+function getFormattedDateList() {
+  const now = new Date();
+  const month = String(now.getUTCMonth() + 1).padStart(2, '0');
+  const day = String(now.getUTCDate()).padStart(2, '0');
+  const hours = String(now.getUTCHours()).padStart(2, '0');
+  const minutes = String(now.getUTCMinutes()).padStart(2, '0');
+
+  return [`${day}/${month}`, `${hours}:${minutes}`];
+}
 
 app.use(express.static(__dirname));
 
@@ -138,7 +256,15 @@ app.get('/player', (req, resp) => {
       const playersjson = JSON.parse(data);
       if (uuid in playersjson) {
         const placement = getKeyPlacement(playersjson, uuid);
-        resp.send(`${playersjson[uuid][0]} - ${placement} - ${playersjson[uuid].length - 1}`);
+        const cardImage = await generateImage({
+          playerName: playersjson[uuid][0],
+          placement: placement,
+          potatoesCollected: playersjson[uuid].length - 1,
+          playerUUID: uuid,
+        });
+
+        resp.set('Content-Type', 'image/png');
+        resp.send(cardImage);
       }
       else {
         resp.send();
