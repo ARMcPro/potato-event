deb6aebe06d6af8d0a1b1d6692c441ae75683f09
ARMcPro <pdarhax@hotmail.com>
2024-05-31 16:56:12 +0300

    Last chance to look at me hector

--- hints.js
+++ hints.js
@@ -1,8 +1,310 @@
 const text = `
 
-Come back at:
-31/05/2024
-14:00 UTC
+Slamdown::
+-Easy:
+Death Dealer Decathlon
+Point Pursuit Path
+Anvil Gold Hoarder
+Sky's Majesty Dunk
+-Medium:
+Single Choice Champion
+Archon's Triumph Unleashed
+Pixie's Triple Triumph
+-Hard:
+Glory after the power
+Twilight Victor Pinnacle
+-Super Hard:
+Courtside Transcendental Trebuchet 
+
+Boat 2::
+-Easy:
+Hidden by water
+Hidden by nature
+Hidden in plain sight
+Simple Victory
+-Medium:
+Victory worth celebrating
+Sticky Situation
+Hidden Vault
+-Hard:
+Victory beyond normal
+Hidden higher
+-Super Hard:
+Victory Royale
+
+Medieval Empire::
+-Easy:
+Peacefully expand your territory
+Expand your army
+Better together
+Teamwork 
+-Medium:
+Modern Age
+Threatening army
+Expand your territory the other way
+-Hard:
+Industrial
+Modern Soldier
+-Super Hard:
+Coal Empire
+
+Caesium::
+-Easy:
+Double Century Coin Collector
+Explosive Victory
+Flag come back
+Mr Burns Simulator
+-Medium:
+Berry Blast Master
+Ambassador of the Electron’s Era
+-Hard:
+Obsidian’s addiction
+Flag Run Glory
+-Super Hard:
+Roseate Era's Mysterious Patron
+
+Rising Lava is Fun::
+-Easy:
+Your First Prey
+Conqueror of the Lava’s lake
+Life is expanding!  
+Onlooker's Inferno Target
+-Medium:
+Create the point
+Dual Deathstroke Destiny
+Nidavellir’s creation
+-Hard:
+The most unlikely item in the game
+All the chances are against me
+-Super Hard:
+Hot potato (too easy?)
+
+Bedwars*::
+-Easy:
+Untimely death
+One less for the Victory
+Boundless Woodland 
+Endless pain
+-Medium:
+Expensive Spud
+Level up
+It’s time to wake up
+-Hard:
+Omnipotent Ore Excavator
+Harder than Rock
+-Super Hard:
+Efficient Saboteur
+
+Bowsenal 2::
+-Easy:
+We win these
+Shock Therapy 
+Life Drained
+Quite Shocking 
+-Medium:
+Among the many Relics 
+Last Chance to kill
+Marked for Death 
+-Hard:
+The Uninterrupted
+One Shot
+-Super Hard:
+Grim Piercer
+
+Deathrun Classic*::
+-Easy:
+Runner success
+Soul Reaper
+Overthrow Hades
+Blasted 
+-Medium:
+Way too triggered
+Omnia win
+Run or hide or die?
+-Hard:
+Untouched Champion
+There are babies in the Basament.... Quick! I have to save them
+-Super Hard:
+The Grassy run
+
+BlockParty*::
+-Easy:
+Terrorist Feats
+Platform Mastery Marvel
+The most famous meme on the Internet
+Third success
+-Medium:
+Champion over the Quartet
+Melodic 
+Doppelganger offense
+-Hard:
+Pentadic Success Sequence
+Perseverance pays off 
+-Super Hard:
+_Jeb and... ? 
+
+Trivia Murder Party*::
+-Easy:
+Password (un)Protected 
+Runaways
+Postmortal
+Deathproof
+-Medium:
+Modern Day Houdini  
+One Sided
+Clairvoyant
+Quiplash!
+-Hard:
+Gambling Addiction
+Feels Good to Be A-Five
+-Super Hard:
+Designated Survivor
+
+Word Bomb::
+-Easy:
+Alphabet Ace
+It’s the name of the game
+Speed Demon
+Deca-Wordsmith
+-Medium:
+Anagram Achiever
+Turtle Typist
+Only love you will ever see
+-Hard:
+Vowel Virtuoso
+Ascending Assembly
+-Super Hard:
+Greedy Overkill
+
+Crossover*::
+-Easy:
+Sky High
+Victory
+This Isn’t sputt time
+Sniper
+-Medium:
+So Below
+Flawless
+Juggernaut
+-Hard:
+Stolen Victory
+You're Going Down Too
+-Super Hard:
+Against All Odds
+
+Battle Box::
+-Easy:
+Winner
+Vote
+Well-versed Combatant
+Builder
+-Medium:
+World Champion
+Flawless
+Top Sneaky
+-Hard:
+Ace
+Literally Find The Potatoes
+-Super Hard:
+Undying Squadron
+
+Deep Echo::
+-Easy:
+Potato Design
+Anybody out there?
+Desolation, all around
+We were here
+-Medium:
+Christmas came late
+Deep beneath the earth
+Is this broken?
+-Hard:
+Gods and Kings
+Free of Burden
+-Super Hard:
+A happy ending?
+
+Bending arena::
+-Easy:
+Obstruction at Birthplace
+Air Temple
+Spirit Oasis 
+5 steps before the underground's world
+-Medium:
+Route Ender
+Warm Looking 
+Metropolitan Mirage
+-Hard:
+The Secret of the Fire
+Twitter hit journey
+-Super Hard:
+Glorius despite difficulties
+
+Randomly Generated Droids::
+-Easy:
+Hunt down an altered one.
+Enter the water into the old world.
+Negotiate a deal to end your hunger
+End the life of an innocent
+-Medium:
+Defeat her.
+Become one with the potato
+Show your gluttony to the potato.
+-Hard:
+Prove Khaos wrong
+Appreciate fast foods.
+-Super Hard:
+Defeat the world, altered by the influence
+
+West::
+-Easy:
+Paycheck Time!
+Good Samaritan
+Advertise go Big
+Less than 21
+-Medium:
+Bandit Buster
+Salty Surprise
+Ironman’s favorite meal
+-Hard:
+Saltcave's Secrets
+Esteem Peak
+-Super Hard:
+Serving the West
+Wretched West
+
+Pixelfields::
+-Easy:
+Explosive Canvas
+Decapitated owner
+The waterfall is hiding my biggest secret
+It seems easy in this way
+-Medium:
+Ouch, that hurt
+24-Hour Spud?!
+Questionable time
+-Hard:
+Chrono Destroyer
+Arm  Designer
+-Super Hard:
+Oversize Potato
+Calamity
+
+Mob Master::
+-Easy:
+Upgrade Squad Sprint
+For the King!
+Forest Lurkers
+Monster Slayer
+-Medium:
+Ascension Squad Achievement
+Pro Gamer
+Centurion's Challenge
+-Hard:
+Medieval conqueror
+Massacre Marathon
+-Super Hard:
+Necromancer 
 
 
 `.trim();
@@ -16,7 +318,7 @@ let currentPlotName = '';
 let hints = '';
 
 filteredLines.forEach(line => {
-  if (line.endsWith(':')) {
+  if (line.endsWith('::')) {
     if (currentPlotName !== '') {
       const row = document.createElement('tr');
 
@@ -32,7 +334,7 @@ filteredLines.forEach(line => {
       tableBody.appendChild(row);
     }
 
-    currentPlotName = line.replace(':', '');
+    currentPlotName = line.replace('::', '');
     hints = '';
   } else {
     hints += line + '<br>';
--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 require("dotenv").config();
+console.log(process.env)
 run = ""
 
 const app = express();
